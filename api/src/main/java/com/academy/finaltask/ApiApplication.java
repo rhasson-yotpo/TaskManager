cc0fc9dacf0898ed9662672097b67f95a6437b46
Ron Hasson <rhasson@yotpo.com>
2022-01-27 00:00:10 +0200

    fix(docker): api waits for db to load, able to use docker-compose

--- api/src/main/java/com/academy/finaltask/ApiApplication.java
+++ api/src/main/java/com/academy/finaltask/ApiApplication.java
@@ -1,17 +1,40 @@
 package com.academy.finaltask;
 
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.retry.annotation.Retryable;
+import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.support.DatabaseStartupValidator;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.stream.Stream;
 
 
 @SpringBootApplication
-@EnableRetry
 public class ApiApplication {
 
-    @Retryable(value = {RuntimeException.class})
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
+
+    @Bean
+    public DatabaseStartupValidator databaseStartupValidator(DataSource dataSource) {
+        var dsv = new DatabaseStartupValidator();
+        dsv.setDataSource(dataSource);
+        return dsv;
+    }
+
+    @Bean
+    public static BeanFactoryPostProcessor dependsOnPostProcessor() {
+        return bf -> {
+
+            String[] jpa = bf.getBeanNamesForType(EntityManagerFactory.class);
+            Stream.of(jpa)
+                    .map(bf::getBeanDefinition)
+                    .forEach(it -> it.setDependsOn("databaseStartupValidator"));
+        };
+    }
+
 }
